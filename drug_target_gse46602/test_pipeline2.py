1e5cad9b188fbc298b2f18b7cc50fd9ac2bbc3cf
Natasha Jaffe <tcjaffe@gmail.com>
2025-04-24 11:36:08 -0400

    Add some explanatory comments.

--- drug_target_gse46602/test_pipeline2.py
+++ drug_target_gse46602/test_pipeline2.py
@@ -6,18 +6,21 @@ import filecmp
 class TestPipeline2:
 
     def test_generate_probe_mappings_csv(self):
+        # Given an input file, probes_ids.txt, containing a list of probes
         test_resources_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_resources")
         probes_file = os.path.join(test_resources_dir, "probe_ids.txt")
         expected_output_file = os.path.join(test_resources_dir, "probe_mapping.csv")
+        temp_dir = tempfile.mkdtemp()
+        output_file = os.path.join(temp_dir, "probe_mapping.csv")
 
         try:
-            temp_dir = tempfile.mkdtemp()
-            output_file = os.path.join(temp_dir, "probe_mapping.csv")
-
+            # When I attempt to generate a mappings file from those probes
             pipeline2.generate_probe_mappings_csv(probes_file, output_file)
 
+            # Then the outputted mapping file should match what we have recorded from a previous run.
             assert(filecmp.cmp(expected_output_file, output_file))
 
         finally:
+            # Cleanup temp files
             os.remove(output_file)
             os.rmdir(temp_dir)
